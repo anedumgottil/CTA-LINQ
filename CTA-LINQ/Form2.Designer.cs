ff8afdcc02a98ca9d8e68315c01a9ee78e59c3c8
Waruan <Waruan5@gmail.com>
2015-11-27 01:40:55 -0600

    fixed graph

--- CTA-LINQ/Form2.Designer.cs
+++ CTA-LINQ/Form2.Designer.cs
@@ -48,8 +48,10 @@
       this.chart1.Location = new System.Drawing.Point(281, 40);
       this.chart1.Name = "chart1";
       series1.ChartArea = "ChartArea1";
+      series1.IsVisibleInLegend = false;
       series1.Legend = "Legend1";
       series1.Name = "Test";
+      series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
       this.chart1.Series.Add(series1);
       this.chart1.Size = new System.Drawing.Size(573, 363);
       this.chart1.TabIndex = 0;
