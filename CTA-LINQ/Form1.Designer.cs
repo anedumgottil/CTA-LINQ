c398b32fdbb7e307d2c998703f8a53b015651eac
Waruan <Waruan5@gmail.com>
2015-11-30 16:19:54 -0600

    remove panel

--- CTA-LINQ/Form1.Designer.cs
+++ CTA-LINQ/Form1.Designer.cs
@@ -66,7 +66,6 @@
       this.textBox2 = new System.Windows.Forms.TextBox();
       this.label6 = new System.Windows.Forms.Label();
       this.label7 = new System.Windows.Forms.Label();
-      this.panel1 = new System.Windows.Forms.Panel();
       this.menuStrip1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.Handicap)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.CompassBox)).BeginInit();
@@ -107,14 +106,14 @@
       // placeholderToolStripMenuItem
       // 
       this.placeholderToolStripMenuItem.Name = "placeholderToolStripMenuItem";
-      this.placeholderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.placeholderToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
       this.placeholderToolStripMenuItem.Text = "About";
       this.placeholderToolStripMenuItem.Click += new System.EventHandler(this.placeholderToolStripMenuItem_Click);
       // 
       // sourceToolStripMenuItem
       // 
       this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
-      this.sourceToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.sourceToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
       this.sourceToolStripMenuItem.Text = "Source";
       this.sourceToolStripMenuItem.Click += new System.EventHandler(this.sourceToolStripMenuItem_Click);
       // 
@@ -378,21 +377,12 @@
       this.label7.TabIndex = 31;
       this.label7.Text = "Avg Ridership";
       // 
-      // panel1
-      // 
-      this.panel1.BackColor = System.Drawing.Color.Aqua;
-      this.panel1.Location = new System.Drawing.Point(622, 30);
-      this.panel1.Name = "panel1";
-      this.panel1.Size = new System.Drawing.Size(279, 111);
-      this.panel1.TabIndex = 32;
-      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.SystemColors.Control;
       this.ClientSize = new System.Drawing.Size(913, 539);
-      this.Controls.Add(this.panel1);
       this.Controls.Add(this.label7);
       this.Controls.Add(this.label6);
       this.Controls.Add(this.textBox2);
@@ -464,7 +454,6 @@
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.Label label6;
     private System.Windows.Forms.Label label7;
-    private System.Windows.Forms.Panel panel1;
   }
 }
 
